98df9c9a81a150693fdcecbacc2a472ce41c0714
superman-enamy <47158027+superman-enamy@users.noreply.github.com>
2023-11-25 07:26:02 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,18 +1,49 @@
-from flask import Flask, render_template
+# api/get_channel_data.py
 
-app = Flask(__name__)
+from flask import jsonify
+import requests
+import gzip
+import xml.etree.ElementTree as ET
+import json
 
+def download_and_unzip(url):
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
 
-@app.route('/')
-def hello():
-    return 'Hello, world'
+    with gzip.open(response.raw, 'rt', encoding='utf-8') as gz_file:
+        xml_content = gz_file.read()
 
+        root = ET.fromstring(xml_content)
+        programs = []
 
-@app.route('/test')
-def test():
-    return 'Test'
+        for program_elem in root.findall('.//programme'):
+            program_data = {
+                'start': program_elem.get('start'),
+                'stop': program_elem.get('stop'),
+                'channel': program_elem.get('channel'),
+                'catchup-id': program_elem.get('catchup-id'),
+                'title': program_elem.find('title').text,
+                'desc': program_elem.find('desc').text,
+                'icon': program_elem.find('icon').get('src')
+            }
 
-@app.route('/result')
-def result():
-   dict = {'phy':50,'che':60,'maths':70}
-   return render_template('result.html', result = dict)
+            programs.append(program_data)
+
+        return programs
+
+def handler(request):
+    # Extract parameters from the request
+    channel_id = request.query.get('id')
+    start_prefix = request.query.get('date')
+
+    # URL to your epg.xml.gz file
+    epg_url = 'https://raw.githubusercontent.com/mitthu786/tvepg/main/tataplay/epg.xml.gz'
+
+    # Call the function to get program data
+    programs = download_and_unzip(epg_url)
+
+    # Filter programs for the specified channel ID and date
+    channel_programs = [program for program in programs if program['channel'] == channel_id and program['start'].startswith(start_prefix)]
+
+    # Return JSON response
+    return jsonify(channel_programs)
